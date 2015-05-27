5f6359c86f5007f32c11c2d24c2b59d7be359d77
Tom MacWright <tom@macwright.org>
2015-05-27 09:46:22 -0400

    Document and add getter

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-module.exports = require('./globals-docs.json');
+var docs = require('./globals-docs.json');
+
+/**
+ * Docs: an object of documentation as a plain-old-javascript object.
+ *
+ * Has keys that correspond to environments:
+ *
+ * - builtin
+ * - nonstandard
+ * - browser
+ * - worker
+ * - node
+ */
+module.exports.docs = docs;
+
+/**
+ * Get a URL for a global object.
+ *
+ * @param {string} name name of the global object
+ * @param {Array<string>} env environments that will be reached. By default tries all environments
+ * @returns {string|undefined} the URL of the documentation resource, if found
+ * @example
+ * getDoc('Array'); // yields MDC documentation for Array
+ */
+module.exports.getDoc = function(name, env) {
+    if (!env) env = Object.keys(docs);
+
+    for (var i = 0; i < env.length; i++) {
+        var d = docs[env[i]][name];
+        if (d) return d;
+    }
+};
--- test.js
+++ test.js
@@ -2,6 +2,10 @@ var test = require('tape'),
   g = require('./');
 
 test('globals-docs', function(t) {
-  t.equal(typeof g, 'object');
+  t.equal(typeof g.docs, 'object');
+
+  t.equal(g.getDoc('Array'), 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array', 'Array');
+  t.equal(g.getDoc('Buffer'), 'https://nodejs.org/api/buffer.html', 'Buffer');
+
   t.end();
 });
