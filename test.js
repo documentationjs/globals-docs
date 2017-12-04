5b037d8bd7eac5ab5d7884b74df0371550940d4b
Tom MacWright <tom@macwright.org>
2017-12-04 10:13:00 -0800

    fix: Fix tests, remove unused dependency

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ var test = require('tape'),
 test('globals-docs', function(t) {
   t.equal(typeof g.docs, 'object');
 
-  t.equal(g.getDoc('Array'), 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array', 'Array');
+  t.equal(g.getDoc('Array'), 'https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array', 'Array');
   t.equal(g.getDoc('Buffer'), 'https://nodejs.org/api/buffer.html', 'Buffer');
-  t.equal(g.getDoc('boolean'), 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean', 'boolean lowercase');
-  t.equal(g.getDoc('Boolean'), 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean', 'boolean uppercase');
+  t.equal(g.getDoc('boolean'), 'https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Boolean', 'boolean lowercase');
+  t.equal(g.getDoc('Boolean'), 'https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Boolean', 'boolean uppercase');
 
   t.end();
 });
